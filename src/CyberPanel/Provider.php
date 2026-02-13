f1b8be1305d9d546616bfda1d8595ff6ae2f9da2
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2026-02-13 16:34:11 +0000

    add change primary domain function

--- src/CyberPanel/Provider.php
+++ src/CyberPanel/Provider.php
@@ -16,6 +16,7 @@ use Upmind\ProvisionBase\Helper;
 use Upmind\ProvisionBase\Provider\Contract\ProviderInterface;
 use Upmind\ProvisionBase\Provider\DataSet\AboutData;
 use Upmind\ProvisionProviders\SharedHosting\Category;
+use Upmind\ProvisionProviders\SharedHosting\Data\ChangePrimaryDomainParams;
 use Upmind\ProvisionProviders\SharedHosting\Data\CreateParams;
 use Upmind\ProvisionProviders\SharedHosting\Data\AccountInfo;
 use Upmind\ProvisionProviders\SharedHosting\Data\AccountUsage;
@@ -414,6 +415,14 @@ class Provider extends Category implements ProviderInterface
                 'package' => $newPackage,
             ]);
         }
+    }
+
+    /**
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     */
+    public function changePrimaryDomain(ChangePrimaryDomainParams $params): AccountInfo
+    {
+        $this->errorResult('Operation not supported');
     }
 
     /**
