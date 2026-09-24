2ef74f0ee58b4a483d06eb80a04c6ebc6d329f1a
RoussKS <35-roussks@users.noreply.git.upmind.io>
2026-09-24 12:07:44 +0100

    use constant for max length

--- src/CyberPanel/Provider.php
+++ src/CyberPanel/Provider.php
@@ -315,13 +315,8 @@ class Provider extends Category implements ProviderInterface
         return substr(
             preg_replace('/^[^a-z]+/', '', preg_replace('/[^a-z0-9]/', '', strtolower($base))),
             0,
-            $this->getMaxUsernameLength() - 2
+            self::MAX_USERNAME_LENGTH - 2
         ) . random_int(1, 99);
-    }
-
-    protected function getMaxUsernameLength(): int
-    {
-        return 8;
     }
 
     protected function api(): Api
